d01617b84cdc043ffe8d73ec63949d9981a46f56
Carlos Tiago <ontiago@gmail.com>
2020-04-20 19:29:59 -0300

    Calculadora de salário

---
+++ 02 - calculadora de salários/lib/calculadora_de_salario.rb
@@ -0,0 +1,9 @@
+class CalculadoraDeSalario
+    def calcula_salario funcionario
+        if funcionario.cargo == Cargo::DESENVOLVEDOR
+            return 3200.0 if funcionario.salario > 3000
+            return 1350.0
+        end
+        return 425.0
+    end
+end
---
+++ 02 - calculadora de salários/test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require File.expand_path('lib/calculadora_de_salario.rb',
+                         File.dirname(__FILE__))
+require File.expand_path('lib/funcionario.rb',
+                         File.dirname(__FILE__))
+
+class CalculadoraDeSalarioTest < Test::Unit::TestCase
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 1500.0, Cargo::DESENVOLVEDOR)
+    salario = calculadora.calcula_salario desenvolvedor
+    assert_equal 1500 * 0.9, salario
+  end
+  def teste_deve_calcular_salario_para_desenvolvedores_com_salario_acima_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 4000.0, Cargo::DESENVOLVEDOR)
+    salario = calculadora.calcula_salario desenvolvedor
+    assert_equal 4000 * 0.8, salario
+  end
+  def teste_deve_calcular_salario_para_DBAs_com_salario_abaixo_do_limite
+    calculadora = CalculadoraDeSalario.new
+    desenvolvedor = Funcionario.new('Mauricio', 500.0, Cargo::DBA)
+    salario = calculadora.calcula_salario desenvolvedor
+    assert_equal 500 * 0.85, salario
+  end
+end
