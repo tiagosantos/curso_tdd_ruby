0a1af135e37006c41c2e7894c5e10387b3ac46a9
Carlos Tiago <ontiago@gmail.com>
2020-04-28 23:00:36 -0300

    refatoração

--- 04 - Nota fiscal/lib/geradordenotafiscal.rb
+++ 04 - Nota fiscal/lib/geradordenotafiscal.rb
@@ -4,17 +4,15 @@ require File.expand_path('./notafiscal.rb', File.dirname(__FILE__))
 require File.expand_path('./nf_dao.rb', File.dirname(__FILE__))
 
 class GeradorDeNotaFiscal
-    def initialize(dao, sap)
-        @dao = dao
-        @sap = sap
+    def initialize(acoes)
+        @acoes = acoes
     end
-    def gera(pedido)
+    def gera pedido
         nf = NotaFiscal.new pedido.cliente,
         pedido.valor_total * 0.94, Time.now
 
-        @dao.persiste nf
-        @sap.envia nf
-
-        return nf
+        @acoes.each { |acao| acao.executa nf }
+        
+        nf
     end
 end
--- 04 - Nota fiscal/teste.rb
+++ 04 - Nota fiscal/teste.rb
@@ -5,43 +5,13 @@ require File.expand_path('lib/notafiscal.rb', File.dirname(__FILE__))
 require File.expand_path('lib/geradordenotafiscal.rb', File.dirname(__FILE__))
 
 class GeradorDeNotaFiscalTest < Test::Unit::TestCase
-    def teste_deve_gerar_NF_com_valor_de_imposto_descontado
-        # criando o mock
-        dao = mock
+    def teste_deve_invocar_acoes_posteriores
+        acao1 = mock.tap{|dao| dao.expects(:executa)}
+        acao2 = mock.tap{|sap| sap.expects(:executa)}
 
-        dao.expects(:persiste)
-        sap = mock.tap{|sap| sap.expects(:envia)}
-
-        gerador = GeradorDeNotaFiscal.new dao, sap
+        gerador = GeradorDeNotaFiscal.new [acao1, acao2]
         pedido = Pedido.new('Mauricio', 1000, 1)
 
         nf = gerador.gera pedido
-
-        assert_equal 1000 * 0.94, nf.valor
-    end
-
-    def teste_deve_persistir_NF_gerada
-        # criando o mock
-        dao = mock
-
-        dao.expects(:persiste)
-
-        sap = mock.tap{|sap| sap.expects(:envia)}
-
-        gerador = GeradorDeNotaFiscal.new dao, sap
-        pedido = Pedido.new('Mauricio', 1000, 1)
-
-        nf = gerador.gera pedido
-    end
-
-    def teste_deve_enviar_NF_gerada_para_SAP
-        dao = stub_everything
-        sap = mock.tap{|sap| sap.expects(:envia)}
-
-        gerador = GeradorDeNotaFiscal.new dao, sap
-        pedido = Pedido.new('Mauricio', 1000, 1)
-
-        nf = gerador.gera pedido
-    end
-    
+    end 
 end
